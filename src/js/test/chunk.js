145b21dfb836d30b9a1547b3f28ba00640b54195
Scorik Egor <shirofaii@gmail.com>
2016-07-04 16:05:03 +0300

    fix tests

--- src/js/test/chunk.js
+++ src/js/test/chunk.js
@@ -8,18 +8,18 @@ let bitmap
 
 describe('ChunkedBitmap', function () {
     beforeEach(function() {
-        state = imm.Map()
-        bitmap = new ChunkedBitmap(state)
+        var canvas = {chunks: imm.Map(), defaultBackgroundTile: 0}
+        bitmap = new ChunkedBitmap().on(canvas)
     });
     it('get/set', function () {
-        assert.equal(bitmap.chunkAt(0, 0), null)
+        assert.equal(bitmap.chunkAt(0, 0), bitmap.defaultChunk)
         var chunk = bitmap.newChunk()
-        bitmap.setChunk(-100, 100, chunk)
+        bitmap.applyChunk(-100, 100, chunk)
         assert.notStrictEqual(bitmap.chunkAt(0, 0), chunk)
         assert.strictEqual(bitmap.chunkAt(-100, 100), chunk)
     });
     it('write tiles', function () {
-        assert.isNull(bitmap.getTile(0, 0))
+        assert.equal(bitmap.chunkAt(0, 0), bitmap.defaultChunk)
         for(var i = -10; i < 10; i++ ) {
             for(var j = -10; j < 10; j++ ) {
                 bitmap.setTile(i, j, 1)
@@ -41,41 +41,4 @@ describe('ChunkedBitmap', function () {
             }
         }
     });
-    it('draw line 1', function () {
-        bitmap.line(0, 0, 10, 10, 1)
-        for(var i = 0; i <= 10; i++ ) {
-            for(var j = 0; j <= 10; j++ ) {
-                if(i === j) {
-                    assert.equal(bitmap.getTile(i, j), 1)
-                } else {
-                    assert.notEqual(bitmap.getTile(i, j), 1)
-                }
-            }
-        }
-    });
-    it('draw line 2', function () {
-        bitmap.line(0, 0, -10, -10, 1)
-        for(var i = -10; i <= 0; i++ ) {
-            for(var j = -10; j <= 0; j++ ) {
-                if(i === j) {
-                    assert.equal(bitmap.getTile(i, j), 1)
-                } else {
-                    assert.notEqual(bitmap.getTile(i, j), 1)
-                }
-            }
-        }
-    });
-    it('draw line 3', function () {
-        bitmap.line(-10, -10, 10, 10, 1)
-        for(var i = -10; i <= 10; i++ ) {
-            for(var j = -10; j <= 10; j++ ) {
-                if(i === j) {
-                    assert.equal(bitmap.getTile(i, j), 1)
-                } else {
-                    assert.notEqual(bitmap.getTile(i, j), 1)
-                }
-            }
-        }
-    });
-
 });
